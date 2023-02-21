489e10f65b1e576ce7d393fb6e961b5d9eb371de
HARVEYNASH\tuyencaok <caokimtuyen1722@gmail.com>
2023-02-21 17:50:35 +0700

    api-url

--- src/components/common/error-boundary.tsx
+++ src/components/common/error-boundary.tsx
@@ -4,5 +4,7 @@ export const ErrorBoundary = () => {
     let error = useRouteError();
     console.error(error);
     // Uncaught ReferenceError: path is not defined
-    return <div>Error! </div>;
+    return <section className="spad">
+        <div className="container">{error!.message}
+        </div></section>
 }
