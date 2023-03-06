f718a97787f20cf4c437fb0b82e644e7750f44c8
HARVEYNASH\tuyencaok <caokimtuyen1722@gmail.com>
2023-03-06 19:58:45 +0700

    checkout page

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,6 @@ import './sass/style.scss'
 import { Outlet, useNavigation } from "react-router-dom"
 import { MainLayout } from "./components/layout"
 import PagePreloder from "./components/common/page-preloder"
-import { PayPalScriptProvider } from "@paypal/react-paypal-js";
-import { PAYPAL_CLIENT_ID } from "./constants"
 
 function App() {
 
@@ -18,21 +16,12 @@ function App() {
 
   if (state === 'loading') return <PagePreloder />
 
-  const initialOptions = {
-    "client-id": PAYPAL_CLIENT_ID,
-    currency: "USD",
-    intent: "capture",
-    "data-client-token": "abc123xyz==",
-  };
-
   return (
-    <PayPalScriptProvider options={initialOptions}>
-      <MainLayout>
-        <>
-          <Outlet />
-        </>
-      </MainLayout>
-    </ PayPalScriptProvider>
+    <MainLayout>
+      <>
+        <Outlet />
+      </>
+    </MainLayout>
   )
 }
 
