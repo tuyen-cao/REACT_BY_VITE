922ca0558286145ff74c09511ffeac8f37b177bd
HARVEYNASH\tuyencaok <caokimtuyen1722@gmail.com>
2023-03-07 20:24:27 +0700

    remove unused code

--- src/components/form-controls/RadioField/index.tsx
+++ src/components/form-controls/RadioField/index.tsx
@@ -4,15 +4,17 @@ import parse from "html-react-parser";
 
 export function RadioField({ name, label, id, ...rest }: InputFieldProps) {
 
-    return (<>
-        {label !== undefined &&
-            <label>{parse(label)}
-                <input
-                    name={name} id={id}
-                    type="radio"
-                    {...rest}
-                />
-                <span className="checkmark"></span></label>}
-    </>
+    return (
+        <>
+            {label !== undefined &&
+                <label>{parse(label)}
+                    <input
+                        name={name} id={id}
+                        type="radio"
+                        {...rest}
+                    />
+                    <span className="checkmark"></span>
+                </label>}
+        </>
     )
 }
--- src/components/header/SearchSwitch/index.tsx
+++ src/components/header/SearchSwitch/index.tsx
@@ -3,8 +3,6 @@ import * as React from 'react';
 export default function SearchSwitch() {
     const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault()
-        /* if (document.getElementById('search-model') !== undefined)
-            document.getElementById('search-model').style.display = "block"; */
     }
     return (
         <button className="search-switch border-0 bg-transparent" onClick={handleClick}>
