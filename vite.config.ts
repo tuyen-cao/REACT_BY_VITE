443e33cfa2bb3ead82c26cb2da4898bf7e12d4b8
HARVEYNASH\tuyencaok <caokimtuyen1722@gmail.com>
2023-03-08 16:41:37 +0700

    aaaa

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import path from 'path'
+
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-    ],
-  },
+  plugins: [react(), tsconfigPaths()]
 })
