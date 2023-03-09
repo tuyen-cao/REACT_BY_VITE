7cfded451f7c31b738f24fb9e18bc499560e53d8
HARVEYNASH\tuyencaok <caokimtuyen1722@gmail.com>
2023-03-09 10:54:25 +0700

    lazy load component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
-import "./assets/css/fontAwesome.min.css"
-import './assets/css/elegantIcons.css'
-import "./assets/css/magnificPopup.css"
-import "./assets/css/niceSelect.css"
-import "./assets/css/owl.carousel.min.css"
-import "./assets/css/slicknav.min.css"
+import './assets/css/fontAwesome.min.css';
+import './assets/css/elegantIcons.css';
+import './assets/css/magnificPopup.css';
+import './assets/css/niceSelect.css';
+import './assets/css/owl.carousel.min.css';
+import './assets/css/slicknav.min.css';
 
-import './sass/style.scss'
-import { Outlet, useNavigation } from "react-router-dom"
-import { MainLayout } from "./components/layout"
-import { PagePreloder } from "./components/common"
+import './sass/style.scss';
+import { Outlet, useNavigation } from 'react-router-dom';
+import { MainLayout } from './components/layout';
+import { PagePreloder } from './components/common';
+import { Suspense } from 'react';
 
 function App() {
+    const { state } = useNavigation();
 
-  const { state } = useNavigation()
+    if (state === 'loading') return <PagePreloder />;
 
-  if (state === 'loading') return <PagePreloder />
-
-  return (
-    <MainLayout>
-      <>
-        <Outlet />
-      </>
-    </MainLayout>
-  )
+    return (
+        <Suspense fallback={<PagePreloder />}>
+            <MainLayout>
+                <>
+                    <Outlet />
+                </>
+            </MainLayout>
+        </Suspense>
+    );
 }
 
-export default App
+export default App;
--- src/components/common/LazyLoadComponent/index.tsx
+++ src/components/common/LazyLoadComponent/index.tsx
@@ -1,15 +1,37 @@
-import { PagePreloder } from '@/components/common';
-import { lazy, Suspense } from 'react';
+import { lazy } from 'react';
 
-export default function LazyLoadComponent(componentUrl: string) {
-    const LazyElement = lazy(
-        () => import(/* @vite-ignore */ `${componentUrl}`)
-    );
+export const lazyWithRetries: typeof lazy = (importer) => {
+    const retryImport = async () => {
+        try {
+            return await importer();
+        } catch (error: any) {
+            // retry 5 times with 2 second delay and backoff factor of 2 (2, 4, 8, 16, 32 seconds)
+            for (let i = 0; i < 5; i++) {
+                await new Promise((resolve) =>
+                    setTimeout(resolve, 1000 * 2 ** i)
+                );
+                // this assumes that the exception will contain this specific text with the url of the module
+                // if not, the url will not be able to parse and we'll get an error on that
+                // eg. "Failed to fetch dynamically imported module: https://example.com/assets/Home.tsx"
+                const url = new URL(
+                    error.message
+                        .replace(
+                            'Failed to fetch dynamically imported module: ',
+                            ''
+                        )
+                        .trim()
+                );
+                // add a timestamp to the url to force a reload the module (and not use the cached version - cache busting)
+                url.searchParams.set('t', `${+new Date()}`);
 
-    // Wrapping around the suspense component is mandatory
-    return (
-        <Suspense fallback={<PagePreloder />}>
-            <LazyElement />
-        </Suspense>
-    );
-}
+                try {
+                    return await import(/* @vite-ignore */ url.href);
+                } catch (e) {
+                    console.log('retrying import');
+                }
+            }
+            throw error;
+        }
+    };
+    return lazy(retryImport);
+};
